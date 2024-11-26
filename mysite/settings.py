23eb48013f1cee15dd90fcb43c488e49ccf4ee98
codefarmer-K <zxuan@usm.cl>
2024-11-26 18:39:30 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 """
 Django settings for mysite project.
 
@@ -123,7 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-=======
+
 """
 Django settings for mysite project.
 
@@ -252,4 +252,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
->>>>>>> friend-master
+
