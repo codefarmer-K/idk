3d8c92b9598b9946c842a818a44b476678a1fa92
codefarmer-K <zxuan@usm.cl>
2024-11-30 17:04:35 -0300

    Update forms.py

--- xenofobia/forms.py
+++ xenofobia/forms.py
@@ -2,7 +2,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from xenofobia.models import UserMessage # 替换为你的模型路径
 
+class UserMessageForm(forms.ModelForm):
+    class Meta:
+        model = UserMessage
+        fields = ['name', 'email', 'message']# 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=30, required=True)
