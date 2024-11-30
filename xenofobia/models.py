dba652fb48da7ffe5fd435977b05fc4589ad759e
codefarmer-K <zxuan@usm.cl>
2024-11-30 17:05:02 -0300

    Update models.py

--- xenofobia/models.py
+++ xenofobia/models.py
@@ -1,3 +1,11 @@
+# Create my models .
 from django.db import models
 
-# Create your models here.
+class UserMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
