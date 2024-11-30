1092c435a6eea5a68162d03adf86d974172272e8
codefarmer-K <zxuan@usm.cl>
2024-11-30 17:08:08 -0300

    Update asgi.py

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-"""
-ASGI config for mysite project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.mysite.settings')
-
-application = get_asgi_application()
-=======
-"""
-ASGI config for mysite project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
 
 import os
 
@@ -32,4 +6,5 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_asgi_application()
->>>>>>> friend-master
+
+
