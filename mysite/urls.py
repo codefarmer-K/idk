24ff5887473405c64e756638b89c2fbfb021f1f1
codefarmer-K <zxuan@usm.cl>
2024-12-02 18:17:56 -0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,9 @@
+
 from django.contrib import admin
 from django.urls import path
 from xenofobia import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -12,3 +15,13 @@ urlpatterns = [
     path('salir/', views.salir_view, name='salir'),
     
 ]
+
+
+# 添加静态文件支持
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
+   
