8aef32c2c8fe2c1d57b73a7859fca611565c859c
codefarmer-K <zxuan@usm.cl>
2024-12-02 18:22:20 -0300

    Update admin.py

--- xenofobia/admin.py
+++ xenofobia/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import UserMessage
 from .models import CustomUser
+from .models import Video
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'uploaded_at')  # 显示标题和上传时间
+    search_fields = ('title',)  # 可通过标题搜索
+
 
 admin.site.register(UserMessage)
 admin.site.register(CustomUser)
+admin.site.register(Video, VideoAdmin)
