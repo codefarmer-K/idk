5a2e44485c29510a8a903773f0e8838be9195bcd
codefarmer-K <zxuan@usm.cl>
2024-12-03 13:43:12 -0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,26 @@
+"""
+URL configuration for mysite project.
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path
 from xenofobia import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.admin.views.decorators import staff_member_required
+
 
 
 urlpatterns = [
@@ -15,7 +31,7 @@ urlpatterns = [
     path('', views.inicial_view, name='inicial'),  # 登录页面
     path('register/', views.register_view, name='register'),  # 注册页面
     path('salir/', views.salir_view, name='salir'),
-    
+    path('contar/', views.contar_view, name='contar'),
 ]
 
 
@@ -24,6 +40,9 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+
+
+   
 
 
    
