d0292713984e531c0c02a491e2fb2834ca8d706b
codefarmer-K <zxuan@usm.cl>
2024-12-05 17:36:19 -0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,22 +1,3 @@
-"""
-URL configuration for mysite project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-from xenofobia import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.admin.views.decorators import staff_member_required
@@ -32,7 +13,12 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),  # 注册页面
     path('salir/', views.salir_view, name='salir'),
     path('contar/', views.contar_view, name='contar'),
+    path('enviroment/', views.enviroment_view, name='enviroment'),
+    path('add_comment/<int:post_id>/', views.add_comment, name='add_comment'),
+    path('delete_post/<int:post_id>/', views.delete_post, name='delete_post'),
+    path('delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
 ]
+
 
 
 # 添加静态文件支持
@@ -40,9 +26,3 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-
-
-   
-
-
-   
